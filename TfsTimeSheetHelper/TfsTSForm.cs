d08ad0a1c8b565891225699e1e13b505ce686889
aniketroxx <aniketp563@gmail.com>
2017-08-13 16:10:48 +0530

    Add CSV Format

--- TfsTimeSheetHelper/TfsTSForm.cs
+++ TfsTimeSheetHelper/TfsTSForm.cs
@@ -2,15 +2,45 @@
 using Microsoft.TeamFoundation.Client;
 using Microsoft.TeamFoundation.WorkItemTracking.Client;
 using System.Net;
+using System.Text;
 using System;
 
 namespace TfsTimeSheetHelper
 {
     public partial class TfsTimeSheetForm : Form
     {
+        StringBuilder csvExport = new StringBuilder();
+
         public TfsTimeSheetForm()
         {
             InitializeComponent();
+        }
+
+        public void addCSVPre()
+        {
+            csvExport.AppendLine("START_HEADER");
+            csvExport.AppendLine("Overriding Approver,,,,,,,,,,,,,,,,,");
+            csvExport.AppendLine("Comments,,,,,,,,,,,,,,,,,");
+            csvExport.AppendLine("STOP_HEADER");
+            csvExport.AppendLine(",,,,,,,,,,,,,,,,,");
+
+            csvExport.AppendLine("START_TEMPLATE");
+            csvExport.AppendLine("Project,Task,Type,Mon,CommentText,Tue,CommentText,Wed,CommentText,Thu,CommentText,Fri,CommentText,Sat,CommentText,Sun,CommentText,END_COLUMN");
+        }
+
+        public void addCSVPost()
+        {
+            csvExport.AppendLine("STOP_TEMPLATE");
+            csvExport.AppendLine("ORACLE RESERVED SECTION");
+            csvExport.AppendLine(",,,,,,,,,,,,,,,,,");
+            csvExport.AppendLine("START_ORACLE");
+
+            csvExport.AppendLine("A|PROJECTS|Attribute1|A|PROJECTS|Attribute2|AE|PROJECTS|Attribute3|PROJECTS|Attribute5|D|DI|CommentText|D|DI|CommentText|D|DI|CommentText|D|DI|Comm");
+            csvExport.AppendLine("entText|D|DI|CommentText|D|DI|CommentText|D|DI|CommentText|,,,,,,,,,,,,,,,,,");
+
+            csvExport.AppendLine("2121472,,,,,,,,,,,,,,,,,");
+            csvExport.AppendLine("A|APPROVAL|Attribute10|DI|CommentText|,,,,,,,,,,,,,,,,,");
+            csvExport.AppendLine("STOP_ORACLE,END");
         }
 
         private void btnGenCSV_Click(object sender, EventArgs e)
